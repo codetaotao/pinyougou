242bfb4438ba88c666236cf15728c753b3ad5be0
admin <2545205694@qq.com>
2019-06-29 01:18:38 +0800

    品优购商城环境搭建,品牌列表测试

---
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,14 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌接口
+ * @author 25452
+ *
+ */
+public interface BrandService {
+	public List<TbBrand> findAll();
+}
