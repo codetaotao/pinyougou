84ae325e3ca38875ab06a88deb926880453d34db
admin <2545205694@qq.com>
2019-06-30 01:08:45 +0800

    更新了品牌的分页

--- pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-parent/pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -9,6 +9,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 
+import entity.PageResult;
+
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
@@ -17,6 +19,10 @@ public class BrandController {
 	@RequestMapping("/findAll")
 	public List<TbBrand> findAll(){
 		return brandService.findAll();
+	}
+	@RequestMapping("/ findPage")
+	public PageResult findPage(int page,int size) {
+		return brandService.findPage(page, size);
 	}
 
 }
--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌接口
  * @author 25452
@@ -11,4 +13,11 @@ import com.pinyougou.pojo.TbBrand;
  */
 public interface BrandService {
 	public List<TbBrand> findAll();
+	/**
+	 * 品牌分页
+	 * @param pageNum 当前页面
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
 }
